62bdbe5eea81ba0c569f3cfddd7e89ae0e68c42a
Behzad2345 <behzadayoub8@gmail.com>
2024-10-29 22:28:08 +0530

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,22 +1,18 @@
 
 //Program in C to display the series 2 4 16 256 65536 ... upto n terms
 
-#include <stdio.h>
+#include<stdio.h>
 #include<math.h>
-int main() {
-   
-   int n;
-    long int term = 2; // starting term
-
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-
-    printf("Series: ");
-    for (int i = 1; i <= n; i++) {
-        printf("%ld ", term);
-        term = term * term; // Square the current term to get the next term
+int main()
+{
+    int n,i,x;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    x=2;
+    printf("%d ",x);
+    for(i=1;i<n;i++){
+        x=pow(x,2);
+        printf(" %d ",x);
     }
-    printf("\n");
-
    return 0;
 }
